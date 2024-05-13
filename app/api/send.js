ab1177ccf34aba394e63e1f9bb7c5c68ae1c22c4
fitgeek96 <fitgeek96@gmail.com>
2024-05-13 03:38:46 +0300

    update

--- app/api/send.js
+++ app/api/send.js
@@ -3,25 +3,27 @@
 import EmailTemplate from "@/pages/Email-template";
 import { Resend } from "resend";
 
-const resend = new Resend("re_fC3rWhKL_NLhiC4577ttuaPfiZWD8Hs1c");
+const resend = new Resend("re_WJAxs4u4_CQVtbqTHyC1SC991BiNpki2R");
 export default async function POST(values) {
   const { nom, prenom, email, phone } = values;
 
-  const { data, error } = await resend.emails.send({
-    from: "Comptoire de Gaume <onboarding@resend.dev>",
-    to: "fitgeek96@gmail.com",
-    subject: "Testing ...",
-    html: `<div>
-            <h3>Nom: ${nom}</h3>
-            <h3>Prenom: ${prenom}</h3>
-            <h3>Email: ${email}</h3>
-            <h3>Tel: ${phone}</h3>
-          </div>`,
-  });
+  try {
+    const { data } = await resend.emails.send({
+      from: "Comptoire de Gaume <onboarding@resend.dev>",
+      to: "fitgeek96@gmail.com",
+      subject: "Testing ...",
+      html: `<div>
+              <h3>Nom: ${nom}</h3>
+              <h3>Prenom: ${prenom}</h3>
+              <h3>Email: ${email}</h3>
+              <h3>Tel: ${phone}</h3>
+            </div>`,
+    });
 
-  if (error) {
-    return Response.json({ error });
+    console.log("Email sent:", data);
+    return { status: "success", message: "Email sent successfully" };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return { status: "error", message: "Error sending email", error };
   }
-
-  return Response.json(data);
 }
